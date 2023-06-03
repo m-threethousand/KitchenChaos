04a33008892784a8904ffccb32a370eb975c9ff2
Mathias <no3000@proton.me>
2023-06-03 12:16:38 +0200

    Init Assets
    Init Assets

---
+++ Assets/Settings/Testing.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Settings{
+    public class Testing : MonoBehaviour{
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
